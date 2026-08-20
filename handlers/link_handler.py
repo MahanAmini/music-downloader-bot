07dcc25d8453b96d78282b0005ee3e638d76aa21
MahanAmini <150641557+MahanAmini@users.noreply.github.com>
2026-08-20 17:38:09 +0330

    Update link_handler.py

--- handlers/link_handler.py
+++ handlers/link_handler.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 import os
 from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
@@ -9,7 +10,6 @@ from services.spotify_service import run_spotify_finder_in_executor
 from services.apple_music_service import apple_music
 
 logger = logging.getLogger(__name__)
-
 
 async def spotify_finder_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     link = update.message.text
@@ -46,6 +46,13 @@ async def spotify_finder_handler(update: Update, context: ContextTypes.DEFAULT_T
         metadata, file_path = result
         logger.info("Track has been detected: %s ---- Successful\n", metadata.name)
 
+        unique_payload = f"{metadata.track_id}_{int(time.time())}"
+        deep_link = f"https://t.me/gasslight_lyrics_bot?start={unique_payload}"
+
+        keyboard = [
+            [InlineKeyboardButton("📜 Get Lyrics", url=deep_link)]
+        ]
+        markup = InlineKeyboardMarkup(keyboard)
         try:
             with open(file_path, "rb") as audio_file:
                 await update.message.reply_audio(audio=audio_file, title=metadata.name,
@@ -53,7 +60,8 @@ async def spotify_finder_handler(update: Update, context: ContextTypes.DEFAULT_T
                                                  caption=f"🎵 *{metadata.name}*\n🎤 {', '.join(metadata.artist)}\n💿 {metadata.album}",
                                                  parse_mode=ParseMode.MARKDOWN,
                                                  read_timeout=120,
-                                                 write_timeout=120
+                                                 write_timeout=120,
+                                                 reply_markup=markup
                                                  )
             logger.info("Audio sent successfully: %s\n", file_path)
 
@@ -76,7 +84,7 @@ async def spotify_finder_handler(update: Update, context: ContextTypes.DEFAULT_T
         await processing_msg.delete()
         await update.message.reply_text(f"❌ <b>Download Failed</b>\n\n"
                                         f"<blockquote expandable>🔗 Track Link: https://open.spotify.com/track/{detected_link.track_id}\n\n"
-                                        f"⚠️ We Found The Track, But Audio Fetching Failed Due To Copyright Restrictions.\n"
+                                        f"⚠️ We Found The Track, But Audio Fetching Failed Due To Copyright Restrictions.\n\n"
                                         f"🔄 Please Try Again In A Few Moments, Or Try A Different Song!</blockquote>",
                                         parse_mode=ParseMode.HTML,
                                         link_preview_options=LinkPreviewOptions(is_disabled=True))
